e408c23edae74b034d98724f59046c35eabfecf0
jonathan murphy <jonathan@jmurphy.me>
2017-11-24 17:27:33 -0500

    testing

--- test_file.py
+++ test_file.py
@@ -1,6 +1,11 @@
 import map_class as wum
+import random
 
-a = wum.Wumpus(f=3,w=2,b=3, l=15, h=15)
+a = wum.Wumpus(f=70,w=6,b=3, l=100, h=150)
+for x in range(10000):
+    x_coord = random.randrange(0, a.get_length())
+    y_coord = random.randrange(0, a.get_height())
+    a.get_map_data(x_coord, y_coord)
 a.get_map_data(1,3)
 a.get_map_data(2,3)
 a.get_map_data(2,4)
@@ -10,3 +15,4 @@ a.get_map_data(4,5)
 a.get_map_data(4,6)
 a.get_map_data(5,6)
 a.print_all()
+a.graph()
