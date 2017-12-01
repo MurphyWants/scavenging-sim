19cc77185949b648ee4b35c5ab5f851fc009cf69
jonathan murphy <jonathan@jmurphy.me>
2017-12-01 14:12:47 -0500

    updated test file

--- test_file.py
+++ test_file.py
@@ -1,18 +1,11 @@
 import map_class as wum
 import random
+import sim_class as sim
 
-a = wum.Wumpus(f=70,w=6,b=3, l=100, h=150)
-for x in range(10000):
-    x_coord = random.randrange(0, a.get_length())
-    y_coord = random.randrange(0, a.get_height())
-    a.get_map_data(x_coord, y_coord)
-a.get_map_data(1,3)
-a.get_map_data(2,3)
-a.get_map_data(2,4)
-a.get_map_data(3,4)
-a.get_map_data(3,5)
-a.get_map_data(4,5)
-a.get_map_data(4,6)
-a.get_map_data(5,6)
+a = wum.Wumpus(f=6,w=3,b=1, l=50, h=75)
 a.print_all()
-a.graph()
+s = sim.Simulation(a, bot_number=50)
+s.make_graph()
+s.generate_random_heatmap(100000)
+s.scatter_bots()
+s.make_graph()
